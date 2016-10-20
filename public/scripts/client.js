bbbbeb8857ae5657fb7a2c0c65658ff453324579
Julie S Mike <juliesmike@gmail.com>
2016-10-20 11:01:19 -0500

    Base project

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,40 +3,40 @@ var app = angular.module('myApp', []);
 app.controller('MainController', MainController);
 
 var API = 'http://api.giphy.com/v1/gifs/';
+var key = 'api_key=dc6zaTOxFJmzC';
+var random = 'random?';
+var search = 'search?q='
 
-var key = '=dc6zaTOxFJmzC';
-//'http://api.giphy.com/v1/';
 
 
-var random = 'random?api_key';
 function MainController($http) {
-
     var main = this;
     main.randomImage = {};
-
+    main.searchImage = [];
     //console.log('MainController loaded');
-    //main.giphy = [{title: 'A New Hope'}, {title: 'The Phantom Menace'}];
 
     //$http get calls
     main.getGiphy = function() {
         //console.log('inside getGiphy function');
-
         $http.get(API + random + key)
             .then(function(response) {
                 console.log('response', response);
                 main.randomImage = response.data.data;
                 console.log('main random image', main.randomImage);
-
-
             });
     }; //end of getGiphy
 
-    //$http get call to API
 
-    //path to search     /v1/gifs/search
+    main.searchGiphy = function() {
+        $http.get(API + search + main.searched + '&' + key)
+            .then(function(response) {
+                // console.log('search response', response);
+                main.searchImage = response.data.data;
+                console.log('main search image', main.searchImage);
+              
 
-    //public beta key       dc6zaTOxFJmzC
+            });
+    }; //end of searchGiphy
 
-    //key of params in config object set to key =
 
 } //end MainController
